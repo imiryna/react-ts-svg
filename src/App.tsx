f761c313cb0d249f09da5cdc26b03653874e2d31
Iryna Burlaka <i.am.blinova@gmail.com>
2024-03-07 13:54:52 +0100

    app init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <button type="button">
+          <p> Learn React</p>
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
